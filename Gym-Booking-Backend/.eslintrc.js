61689944fb61dd7ce896e3e499e65b2f9af072d1
rishi <prabhak8@uni.coventry.ac.uk>
2020-07-12 01:37:01 +0100

    user can now register

--- Gym-Booking-Backend/.eslintrc.js
+++ Gym-Booking-Backend/.eslintrc.js
@@ -3,19 +3,23 @@ module.exports = {
     'browser': true,
     'commonjs': true,
     'es2020': true,
-    'node': true
+    'node': true,
   },
   'extends': [
     'prettier',
     'google',
   ],
-  'plugins':[
-    'prettier'
+  'plugins': [
+    'prettier',
   ],
   'parserOptions': {
     'ecmaVersion': 11,
   },
   'rules': {
-    'linebreak-style': 0
+    'linebreak-style': 0,
+    'new-cap': 0,
+    'camelcase': 0,
+    'max-len': 0,
+    'valid-jsdoc': 0,
   },
 };
--- Gym-Booking-Backend/index.js
+++ Gym-Booking-Backend/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
-const mysql = require('mysql');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'gym_booking_db',
-});
-
-db.connect();
+app.use(express.json());
+app.use('/register/', require('./Routes/Register'));
 
 app.get('/', (req, res) => {
   res.send('Welcome to Gym Bookings');
