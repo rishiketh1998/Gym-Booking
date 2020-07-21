e2d575c573f612e9bae37fc4ac21214408867dba
rishi <prabhak8@uni.coventry.ac.uk>
2020-07-21 20:31:32 +0100

    updated banner tests and updated logo for navbar

--- digital-gym/src/Home_Page/Home.js
+++ digital-gym/src/Home_Page/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Banner} from "./Banner";
+import {Header_Nav} from "./Header_Nav";
 import {useTimeout} from "../Hooks/useTimeoutHook";
 
 export const Home = () => {
     const {timer} = useTimeout(true,4000)
     return (
-        <div>
-            <Banner display={timer}/>
-        </div>
+        <>
+            {timer && <Banner />}
+            <Header_Nav />
+        </>
     )
 }
